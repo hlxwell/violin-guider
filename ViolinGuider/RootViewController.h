cc4b97e9a07124ab5bb820924493e6ed1b075657
Michael He <hlxwell@gmail.com>
2012-04-07 15:13:46 +0800

    Initial Commit

---
+++ ViolinGuider/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  ViolinGuider
+//
+//  Created by michael he on 4/7/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
